8c1e510b6bd9d28dba2108c6c4f874fd1bb6de00
TRI_SEVEN <chenzhiyuan77@163.com>
2020-07-14 15:14:38 +0800

    第一次提交，主要是第二章IDE之前的内容

---
+++ ch2/build/CMakeFiles/usehello_shared.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usehello_shared.dir/usehello.cpp.o"
+  "usehello_shared.pdb"
+  "usehello_shared"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usehello_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch2/helloSLAM.cpp
@@ -0,0 +1,9 @@
+//2020/13/07/ 16:59:45
+ 
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+	cout<<"Hello SLAM!"<<endl;
+	return 0;
+}
